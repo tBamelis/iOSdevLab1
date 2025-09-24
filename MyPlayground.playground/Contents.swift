d345727997bc953844932de4a85d1efd933a18fb
Thibeau Bamelis <thibeau@iMacD.local>
2025-09-24 09:39:14 +0200

    first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var text = "SWIFT";
+let max = 10;
+var average = 0.0;
+
+for letter in text.unicodeScalars
+{
+    print(letter);
+}
+
+var tuple = (voornaam: "thibeau", naam: "bamelis");
+print(tuple.voornaam)
+print(tuple.naam)
+
+
+var reversedString: String? = NILL;
+
+
+
+
+
+
